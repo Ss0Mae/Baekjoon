919c61cabc5baceb5a2a5b1bb983349e7e326258
ssomae <80831228+Ss0Mae@users.noreply.github.com>
2024-07-22 15:35:21 +0900

    [level 1] Title: 예산, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.py
@@ -0,0 +1,8 @@
+def solution(d, budget):
+    answer = 0 # 가능한 부서의 수
+    for i in sorted(d):
+        budget -= i
+        if budget < 0: 
+            break
+        answer += 1
+    return answer
