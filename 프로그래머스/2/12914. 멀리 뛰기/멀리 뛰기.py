5604005217a0104e596659baf0e470a5040dc6ce
ssomae <80831228+Ss0Mae@users.noreply.github.com>
2025-03-01 11:12:19 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.20 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -1,9 +1,6 @@
 def solution(n):
-    answer = 0
-    a, b = 0,1
-    
-    for i in range(n) :
-        a,b = b, a+b
-    
-    answer = b % 1234567
-    return answer
+    mod = 1234567
+    prev, curr = 0, 1
+    for _ in range(n):
+        prev, curr = curr, (prev + curr) % mod
+    return curr
