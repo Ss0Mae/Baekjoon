cb0bb044f0e847de24f65ec24c366eb66df1f1a5
ssomae <80831228+Ss0Mae@users.noreply.github.com>
2025-03-01 11:01:55 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.03 ms, Memory: 9.99 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,17 +1,17 @@
-# 내가 작성한 문제 풀이
 def solution(n, words):
-    used_words = []
-    number, order = 0,0
-
-    used_words.append(words[0])
-    last_word = words[0][-1]
-    for i in range(1,len(words)):
-        if words[i] not in used_words and words[i][0] == last_word:
-            used_words.append(words[i])
-            last_word = words[i][-1]
-        else:
-            number = (i%n)+1
-            order = (i//n)+1
-            break
+    # 첫 단어를 처리: 집합에 저장하고 마지막 글자 기록
+    used_words = {words[0]}
+    last_char = words[0][-1]
     
-    return [number, order]
+    # 두 번째 단어부터 처리
+    for i, word in enumerate(words[1:], start=1):
+        # 이미 사용된 단어거나 앞 단어의 마지막 글자와 맞지 않는 경우
+        if word in used_words or word[0] != last_char:
+            # 플레이어 번호와 몇 번째 차례인지를 계산하여 반환
+            return [(i % n) + 1, (i // n) + 1]
+        # 현재 단어를 사용된 단어 집합에 추가하고, 마지막 글자 업데이트
+        used_words.add(word)
+        last_char = word[-1]
+    
+    # 모든 단어가 규칙을 지킨 경우
+    return [0, 0]
