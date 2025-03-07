c487b1b01aa47cca5423b5d659bbdbbe5092b439
ssomae <80831228+Ss0Mae@users.noreply.github.com>
2025-03-08 02:17:12 +0900

    [level 2] Title: 할인 행사, Time: 72.70 ms, Memory: 17.4 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/README.md
+++ 프로그래머스/2/131127. 할인 행사/README.md
@@ -1,10 +1,10 @@
 # [level 2] 할인 행사 - 131127 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127?language=python3) 
 
 ### 성능 요약
 
-메모리: 42 MB, 시간: 24.70 ms
+메모리: 17.4 MB, 시간: 72.70 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 04일 23:29:12
+2025년 03월 08일 02:17:10
 
 ### 문제 설명
 
--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -1,16 +1,34 @@
+from collections import Counter
+
 def solution(want, number, discount):
+    # 원하는 제품과 필요한 수량을 딕셔너리로 구성
+    required = {w: n for w, n in zip(want, number)}
+    window_size = sum(number)  # 항상 10
     answer = 0
-    list_all_want = []
+    
+    # 할인되는 제품 목록이 window_size보다 작으면 조건을 만족할 수 없으므로 0 리턴
+    if len(discount) < window_size:
+        return 0
 
-    for i in range(len(want)):
-        for j in range(number[i]):
-            list_all_want.append(want[i])
-    list_all_want.sort()
+    # 초기 윈도우의 제품 빈도 계산
+    current_window = Counter(discount[:window_size])
+    if current_window == required:
+        answer += 1
 
-    for i in range(len(discount) - 9):
-        list_10 = discount[i: i+10]
-        list_10.sort()
-        if list_all_want == list_10:
+    # 슬라이딩 윈도우를 이용하여 각 구간 검사
+    for i in range(1, len(discount) - window_size + 1):
+        # 윈도우에서 나가는 제품
+        outgoing = discount[i - 1]
+        current_window[outgoing] -= 1
+        if current_window[outgoing] == 0:
+            del current_window[outgoing]
+        
+        # 윈도우에 새로 들어오는 제품
+        incoming = discount[i + window_size - 1]
+        current_window[incoming] += 1
+        
+        # 현재 윈도우의 제품 빈도와 요구 조건 비교
+        if current_window == required:
             answer += 1
 
     return answer
