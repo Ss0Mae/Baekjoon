32e7b2d28d6f90958bba99681c0a25b4c54c99a8
ssomae <80831228+Ss0Mae@users.noreply.github.com>
2025-03-08 02:25:58 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 212.39 ms, Memory: 43.8 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/README.md
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 연속 부분 수열 합의 개수 - 131701 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131701?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131701) 
 
 ### 성능 요약
 
-메모리: 43.7 MB, 시간: 4501.64 ms
+메모리: 43.8 MB, 시간: 212.39 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 06일 23:59:10
+2025년 03월 08일 02:25:57
 
 ### 문제 설명
 
--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -1,11 +1,13 @@
 def solution(elements):
-    result = set()
+    n = len(elements)
+    doubled = elements * 2  # 원형 수열 처리를 위한 더블 배열
+    distinct_sums = set()
     
-    elementLen = len(elements)
-    # [7, 9, 1, 1, 4, 7, 9, 1, 1, 4]
-    elements = elements * 2
-    
-    for i in range(elementLen):
-        for j in range(elementLen):
-            result.add(sum(elements[j:j+i+1]))
-    return len(result)
+    for i in range(n):
+        current_sum = 0
+        # 최대 n개 원소를 더하는 경우까지만 확인
+        for j in range(i, i + n):
+            current_sum += doubled[j]
+            distinct_sums.add(current_sum)
+
+    return len(distinct_sums)
