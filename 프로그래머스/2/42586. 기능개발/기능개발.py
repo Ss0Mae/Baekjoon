29fa1b6ffe5817571a925a59bbda0433a8a41681
ssomae <80831228+Ss0Mae@users.noreply.github.com>
2025-03-14 23:37:54 +0900

    [level 2] Title: 기능개발, Time: 0.05 ms, Memory: 9.01 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,19 +1,23 @@
 def solution(progresses, speeds):
-
     answer = []
-    time = 0
-    count = 0
-    
-    while len(progresses)> 0:
-        if (progresses[0] + time*speeds[0]) >= 100: 
+    days = 0 # 날짜 세기
+    cnt = 0 # 완료된 기능
+    while len(progresses) > 0:
+    	# 기능의 진행상황과 그 동안 지난 날짜만큼의 speed를 구해서 더하기
+        if(progresses[0]+days*speeds[0])>=100:
+            # 완료되면 리스트에서 제거
             progresses.pop(0)
             speeds.pop(0)
-            count += 1
-            
+            # 완료된 기능 수
+            cnt += 1
         else:
-            if count > 0:
-                answer.append(count)
-                count = 0
-            time += 1
-    answer.append(count)
+        	# 만약 완료된 기능이 있다면 answer에 더해주고 0으로 초기화
+            if cnt > 0:
+                answer.append(cnt)
+                cnt = 0
+            # 완료된 기능이 없으면 days 추가
+            else:
+                days+=1
+    answer.append(cnt)
     return answer
+
