0aee7dc08e9064b20c90be72ad7d078cd2d394af
ssomae <80831228+Ss0Mae@users.noreply.github.com>
2025-03-21 22:14:57 +0900

    [level 2] Title: 튜플, Time: 36.84 ms, Memory: 26.3 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.py
+++ 프로그래머스/2/64065. 튜플/튜플.py
@@ -1,12 +1,12 @@
 def solution(s):
-    li=[]
-    for i in s.split("},"):
-        li.append(i.replace("{","").replace("}","").split(","))
-    li.sort(key=len)
-    answer=[]
-    for i in li:
-        for j in i:
-            if j not in answer:
-                answer.append(j)
-    return list(map(int,answer))
-
+    
+    sets = [set(item.split(",")) for item in s[2:-2].split("},{")]
+    # 집합의 크기를 기준으로 정렬
+    sets.sort(key=len)
+    
+    answer = []
+    # 정렬된 집합 순회하며, 새로 등장한 원소만 추가
+    for subset in sets:
+        answer.extend(subset - set(answer))
+    
+    return list(map(int, answer))
