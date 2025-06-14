82f890f04cc4310714efd213d25f8cbc64f9d6d6
ssomae <80831228+Ss0Mae@users.noreply.github.com>
2025-06-14 23:47:24 +0900

    [level 2] Title: 기능개발, Time: 0.45 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,23 +1,21 @@
 def solution(progresses, speeds):
+    #결과를 담을 리스트
     answer = []
-    days = 0 # 날짜 세기
-    cnt = 0 # 완료된 기능
-    while len(progresses) > 0:
-    	# 기능의 진행상황과 그 동안 지난 날짜만큼의 speed를 구해서 더하기
-        if(progresses[0]+days*speeds[0])>=100:
-            # 완료되면 리스트에서 제거
+    #작업 리스트가 빌 때까지 반복
+    while progresses :
+        #몇개의 기능이 배포되는지 저장 
+        cnt = 0
+        #작업 리스트가 남아있고 맨 앞의 작업의 진도가 100인 경우: 기능 배포 변수 증가. 해당 작업과 작업 속도를 리스트에서 제거
+        while progresses and progresses[0] >= 100:
+            cnt+=1
             progresses.pop(0)
             speeds.pop(0)
-            # 완료된 기능 수
-            cnt += 1
-        else:
-        	# 만약 완료된 기능이 있다면 answer에 더해주고 0으로 초기화
-            if cnt > 0:
-                answer.append(cnt)
-                cnt = 0
-            # 완료된 기능이 없으면 days 추가
-            else:
-                days+=1
-    answer.append(cnt)
-    return answer
 
+        # 작업 리스트의 진도를 증가
+        progresses = [progresses[i]+speeds[i] for i in range(len(progresses))]
+
+        #만약 오늘 기능이 배포되었다면 결과리스트에 추가
+        if cnt:
+            answer.append(cnt)
+    
+    return answer
