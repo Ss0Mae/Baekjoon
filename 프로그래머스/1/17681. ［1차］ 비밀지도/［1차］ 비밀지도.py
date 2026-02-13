6d6440ceaf0de75e5058503fd3ee67bcb92f8286
Jo SeongMin <80831228+Ss0Mae@users.noreply.github.com>
2026-02-14 00:31:41 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.02 ms, Memory: 9.15 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -1,19 +1,10 @@
 def solution(n, arr1, arr2):
     answer = []
-    arr1_bin = []
-    arr2_bin = []
     for i in range(n):
-        arr1_bin.append(bin(arr1[i])[2:])
-        arr2_bin.append(bin(arr2[i])[2:])
-        arr1_bin[i] = ('0' * (n-len(arr1_bin[i]))) + arr1_bin[i]
-        arr2_bin[i] = ('0' * (n-len(arr2_bin[i]))) + arr2_bin[i]
-    
-        tmp = ''
-        for p in range(n):
-            if arr1_bin[i][p] == '1' or arr2_bin[i][p] == '1':
-                tmp += '#'
-            elif arr1_bin[i][p] == '0' and arr2_bin[i][p] == '0':
-                tmp += ' '
-        answer.append(tmp)
-        
+        a = bin(arr1[i] | arr2[i])
+        a = a[2:].zfill(n) 
+        # zfill(n) -> zfill()은 문자열 형태에서 지정한 길이만큼 0을 채워          줍니다.단, 지정한 길이는 기존 문자열의 길이를 포함합니다.
+        #print(a)
+        a = a.replace('1', '#').replace('0', ' ')
+        answer.append(a)
     return answer
