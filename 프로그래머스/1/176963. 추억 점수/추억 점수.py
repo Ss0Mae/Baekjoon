ba2868d12e2248db08eb3ce4a018f9cc97a00df8
Jo SeongMin <80831228+Ss0Mae@users.noreply.github.com>
2026-02-25 02:18:40 +0900

    [level 1] Title: 추억 점수, Time: 1.16 ms, Memory: 9.95 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.py
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.py
@@ -2,7 +2,7 @@ def solution(name, yearning, photo):
     # 1. 반환할 결괏값 선언과 이름과 그리움 점수를 묶어준다.
     result = []
     info = dict(zip(name, yearning))
-    
+    #print(info)
     # 2. 사진 속 사람들을 확인할 for loop
     for people in photo:
         
@@ -12,7 +12,7 @@ def solution(name, yearning, photo):
         # 4. 사람들 각각의 점수를 확인할 for loop
         for person in people:
             # 5. 사람 별 점수를 누적, 없는 사람은 0
-            score += info.get(person, 0)
+            score += info.get(person,0)
         # 6. 결과 배열에 담아주기
         result.append(score)
     
