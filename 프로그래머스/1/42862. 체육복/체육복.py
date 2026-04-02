03a6e0e09456bf02ce29c6c913228651e9523915
Jo SeongMin <80831228+Ss0Mae@users.noreply.github.com>
2026-04-02 16:40:06 +0900

    [level 1] Title: 체육복, Time: 0.02 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -3,7 +3,7 @@ def solution(n, lost, reserve):
     reserve = sorted(reserve)
     save = 0
     for l in lost:
-        if l in reserve:
+        if l in reserve: # 잃어버린애가 여벌이 있으면 남은 체육복이 하나이기 때문에 수업을 들을수 있음
             save+=1
             reserve.remove(l)
             continue
